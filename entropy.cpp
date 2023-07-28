536278dc1b59f2390b37057b7a66d67eef810607
merces <nandu88@gmail.com>
2023-07-28 09:13:38 -0300

    Zero out variables at every iteration (closes #6)

--- entropy.cpp
+++ entropy.cpp
@@ -4,26 +4,25 @@
 #include <fstream>
 #include <vector>
 
-double calculate_entropy(const unsigned int counted_bytes[256], const std::streamsize total_length) {
+double calculate_entropy(const std::vector<int> counted_bytes, const std::streamsize total_length) {
     double entropy = 0.;
     double temp;
 
     for (int i = 0; i < 256; i++) {
         temp = static_cast<double>(counted_bytes[i]) / total_length;
 
-        if (temp > 0.) {
+        if (temp > 0.)
             entropy += temp * fabs(log2(temp));
-        }
     }
 
     return entropy;
 }
 
 void usage() {
-    std::cout << "entropy calculates the entropy of files, but you need to provide it with a file. :)\n\n"
+    std::cout << "entropy calculates the entropy of files, but you need to provide it with at least one file. :)\n\n"
         "Usage:\n\tentropy FILE\n\n"
         "Examples:\n\tentropy image.png\n\tentropy music.mp3 document.xls\n\tentropy *.exe\n\n"
-        "For more information and bug reporting, refer to https://github.com/merces/entropy\n";
+        "For more information and bug reports, refer to https://github.com/merces/entropy\n";
 }
 
 int main(int argc, char *argv[])
@@ -33,22 +32,21 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Entropy will have two decimal places
+    // Entropy value will use two decimal places
     std::cout << std::fixed << std::setprecision(2);
 
     // 16KB chunks
     std::vector<char> buff(1024*16, 0);
-    std::streamsize total_bytes_read = 0;
+    std::streamsize total_bytes_read;
     std::streamsize bytes_read;
-    unsigned char count;
 
-    // Count occurrence of each possible byte, from zero to 255.
-    unsigned int counted_bytes[256] = {};
+    // This vector will hold how many times every possible byte value occurs
+    std::vector<int> counted_bytes(256);
 
     for (int i = 1; i < argc; i++) {
         // Skip directories, symlinks, etc
         if (!std::filesystem::is_regular_file(argv[i])) {
-            std::cerr << "\"" << argv[1] << "\"" << " isn't a regular file, skipping." << std::endl;
+            std::cerr << "\"" << argv[i] << "\"" << " isn't a regular file, skipping." << std::endl;
             continue;
         }
 
@@ -59,20 +57,24 @@ int main(int argc, char *argv[])
             continue;
         }
 
-        // Read file in chunks and count the occurrences of each possible byte (0-255)
+        // Perform a fresh calculation
+        total_bytes_read = 0;
+        std::fill(counted_bytes.begin(), counted_bytes.end(), 0);
+
+        // Read file in chunks and count the occurrences for each possible byte value (0-255)
         while (!input_file.eof()) {
             input_file.read(buff.data(), buff.size());
             bytes_read = input_file.gcount();
             total_bytes_read += bytes_read;
 
             for (int j = 0; j < bytes_read; j++) {
-                count = static_cast<unsigned char> (buff[j]);
+                const unsigned char count = static_cast<unsigned char> (buff[j]);
                 counted_bytes[count]++;
             }
         }
         input_file.close();
         std::cout << calculate_entropy(counted_bytes, total_bytes_read) << " " << argv[i] << "\n";
     }
+
 	return 0;
 }
-
