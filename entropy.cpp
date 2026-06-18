6da78d387dfbab7248e3b158be93d1381ca06d96
merces <nandu88@gmail.com>
2026-06-18 19:15:19 -0300

    Add copyright notice

--- entropy.cpp
+++ entropy.cpp
@@ -1,4 +1,24 @@
-﻿#include <iostream>
+﻿/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * Copyright (c) 2022-2023, 2025 Fernando Mercês
+ * Copyright (c) 2022 Shader Coder
+ * Copyright (c) 2025 Ivan Volosyuk
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
 #include <cmath>
 #include <filesystem>
 #include <fstream>
